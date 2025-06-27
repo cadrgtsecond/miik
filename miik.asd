272dbbe80c98bc175fc0a15620ac0963ec1501cf
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2025-06-27 14:12:45 +0530

    feat: Initial commit - MIIK Server

---
+++ miik.asd
@@ -0,0 +1,9 @@
+(defsystem "miik"
+  :version "0.0.0"
+  :author "cadrgtsecond"
+  :license ""
+  :homepage ""
+  :depends-on (bordeaux-threads)
+  :serial t
+  :components ((:module "src" :components ((:file "package"))))
+  :description "")
---
+++ src/package.lisp
@@ -0,0 +1,40 @@
+(defpackage miik
+  (:use :cl)
+  (:export start-listening))
+
+(in-package :miik)
+
+(defun repl-loop (connection-path)
+  (alexandria:when-let ((connection-dir (probe-file connection-path)))
+    (print connection-dir)
+    (loop
+      (let ((res (with-open-file (stdin (merge-pathnames #p"stdin" connection-dir))
+                      (with-open-file (*standard-output* (merge-pathnames #p"stdout" connection-dir)
+                                                         :direction :output
+                                                         :if-exists :append)
+                        (eval (read stdin))))))
+        (with-open-file (result (merge-pathnames #p"result" connection-dir))
+          (write res :stream result))))))
+
+(defvar *connection-thread*)
+(defvar *repl-threads* '())
+
+(defun connection-loop ()
+  (loop
+    (with-open-file (fifo #p"/tmp/miikfifo")
+      (alexandria:when-let ((connection-dir (read-line fifo nil)))
+        (push (bt:make-thread (lambda () (repl-loop connection-dir))) *repl-threads*)))))
+
+(defun start-listening ()
+  "Starts listening for incoming connections from /tmp/miikfifo"
+  (uiop:run-program '("mkfifo" "/tmp/miikfifo") :ignore-error-status t)
+  (setf *connection-thread* (bt:make-thread #'connection-loop :name "miik-thread")))
+
+(defun stop-listening ()
+  (when (bt:thread-alive-p *connection-thread*)
+    (bt:destroy-thread *connection-thread*))
+  (loop until (null *repl-threads*)
+        for thread = (car *repl-threads*)
+        do (when (bt:thread-alive-p thread)
+             (bt:destroy-thread thread))
+        do (setf *repl-threads* (cdr *repl-threads*))))
