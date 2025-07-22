db6259c4a19f7677bae28b93188d33f9e872b089
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2025-07-22 13:26:56 +0530

    feat: Use proper error handling

--- src/package.lisp
+++ src/package.lisp
@@ -1,25 +1,38 @@
-(defpackage miik
-  (:use :cl)
-  (:export start stop))
+(defpackage #:miik
+  (:use #:cl)
+  (:export #:start-server #:stop-server #:*real-stdout*))
 
-(in-package :miik)
+(in-package #:miik)
+
+(defvar *real-stdout*)
+
+(defun handle-connection (stream)
+  (format stream "~a~%"
+    (let ((*real-stdout* *standard-output*))
+      (handler-case
+        (with-output-to-string (*standard-output*)
+          (eval (read stream)))
+        (error (e) (format t "miik error: ~a~%" e)))))
+  (force-output stream))
 
 (defun main (host port)
   (usocket:with-socket-listener (listener host port)
     (loop
-      for conn = (usocket:socket-accept listener)
-      for stream = (usocket:socket-stream conn)
-      do (format stream "~a~%"
-                 (with-output-to-string (*standard-output*)
-                   (eval (read stream))))
-      do (force-output stream))))
+      (usocket:with-server-socket (conn (usocket:socket-accept listener))
+        (handle-connection (usocket:socket-stream conn))))))
 
 (defvar *miik-thread* '())
 (defun start-server (&optional (host "localhost") (port 3700))
-  (setf *miik-thread* (bt:make-thread (lambda () (main host port)) :name "miik Server")))
+  (setf *miik-thread* (bt:make-thread (lambda () (main host port)) :name "miik server")))
 
 (defun stop-server ()
   (bt:destroy-thread *miik-thread*))
 
-(defun test ()
-  (print "Hello 2"))
+#+nil
+(progn
+  (defparameter *counter* 0)
+  (defun test ()
+    (format t "Counter: ~a~%" *counter*)
+    (incf *counter*)))
+#+nil
+(test)
